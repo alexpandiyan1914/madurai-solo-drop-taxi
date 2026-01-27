581c5cc58a4636a60e5b195b64991fceabc0e6b5
Alexpandiyan <135411805+alexpandiyan1914@users.noreply.github.com>
2026-01-27 14:37:44 +0530

    Fixing bugs (#7)
    * Made changes in Tariff & Booking form
    
    * Add packages section and enhance Footer section
    
    * Add transitions to Hero & Tariff pages
    
    * Fixing bugs and add AOS  (#6)
    
    * Made changes in Tariff & Booking form (#4)
    
    * Adding Packages Section (#5)
    
    * Made changes in Tariff & Booking form
    
    * Add packages section and enhance Footer section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import "./Hero.css";
+import "./hero.css";
 import { useEffect, useState } from "react";
 
 const Hero = () => {
@@ -9,26 +9,25 @@ const Hero = () => {
       <div className="container hero-content">
         <div className="row align-items-center min-vh-100">
           <div
-            className="col-lg-6 text-white"
-            data-aos="fade-up"
+            className="col-lg-6 text-white" 
           >
-            <h1 className="hero-title">
+            <h1 className="hero-title" data-aos="fade-up" data-aos-delay="100">
               Madurai’s Trusted <br />
-              <span>Solo Drop Taxi Service</span>
+              <span data-aos="fade-up" data-aos-delay="200">Solo Drop Taxi Service</span>
             </h1>
 
-            <p className="hero-subtitle">
+            <p className="hero-subtitle" data-aos="fade-up" data-aos-delay="200">
               One-way taxi rides from Madurai to anywhere in India.
               Clean cars, professional drivers, transparent pricing.
             </p>
 
-            <div className="hero-features">
+            <div className="hero-features" data-aos="fade-up" data-aos-delay="300">
               <span>✔ 24/7 Service</span>
               <span>✔ No Return Fare</span>
               <span>✔ On-Time Pickup</span>
             </div>
 
-            <div className="hero-buttons mt-4">
+            <div className="hero-buttons mt-4"  data-aos="fade-up" data-aos-delay="400">
               <a href="#book" className="btn btn-warning btn-lg me-3">
                 Book Now
               </a>
